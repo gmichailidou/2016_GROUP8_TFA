8ad11316cab9fb22791d833ddacacb3d42faddeb
lydkotoula <lydiakotoula@outlook.com>
2017-01-15 20:56:42 +0100

    report changes 15-1-2017

--- advISOr/spatial_decision_dockwidget.py
+++ advISOr/spatial_decision_dockwidget.py
@@ -22,11 +22,11 @@
 """
 
 from PyQt4 import QtGui, QtCore, uic
-from qgis.core import *                   #error
-from qgis.networkanalysis import *         #error
-from qgis.gui import *                 #error
+from qgis.core import *
+from qgis.networkanalysis import *
+from qgis.gui import *
 import processing
-import webbrowser  #error
+import webbrowser
 
 # matplotlib for the charts
 from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as FigureCanvas
@@ -40,6 +40,10 @@ import os.path
 import random
 import csv
 import time
+#import matplotlib.pyplot as plt
+#import plotly.plotly as py
+#import plotly.plotly as py
+#import plotly.graph_objs as go
 
 from . import utility_functions as uf    # error
 
@@ -91,20 +95,17 @@ class SpatialDecisionDockWidget(QtGui.QDockWidget, FORM_CLASS):
         self.subScenario = {}
 
         # add button icons
-        #self.bigiconButton.setIcon(QtGui.QIcon(':icons/pascal.png'))
+        self.bigiconButton.setIcon(QtGui.QIcon(':icons/advisor1.png'))
         self.bigiconButton.clicked.connect(self.openinBrowser)
 
         # indicators
         self.sliderValue_2.textChanged.connect(self.sliderTextChanged2)
-
         self.selecttimeCombo.activated.connect(self.setTimeSlot)
         self.selecttimeCombo.activated.connect(self.selectfreq)
         self.horizontalSlider.sliderMoved.connect(self.sliderMoved2)
-
-        #self.horizontalSlider.valueChanged.connect(self.sliderValueChanged)
         self.nodesFrequencyLayer.clicked.connect(self.warningFrequencyNodes)
+        self.selecttimeCombo.activated.connect(self.MaxThresholdText)
 
-        #self.agegroupBox.activated.connect(self.setAgeGroup)
 
         # initialize
         #self.sliderInit()
@@ -112,17 +113,20 @@ class SpatialDecisionDockWidget(QtGui.QDockWidget, FORM_CLASS):
         # analysis
         self.sliderValue.textChanged.connect(self.sliderTextChanged)
         self.stationDistanceSlider.sliderMoved.connect(self.sliderMoved)
-        self.bufferbutton.clicked.connect(self.calculateBuffer)
+        self.bufferbutton.clicked.connect(self.warningbuffer)
         self.distanceVisiblecheckBox.stateChanged.connect(self.distanceVisible)
+        self.initialareas.clicked.connect(self.layer_initial)
+        self.criticalarea.clicked.connect(self.layerpopulation)
         #self.initialareasVisiblecheckBox.connect()
         #self.criticalVisiblecheckBox.connect()
         self.screenshotButton.clicked.connect(self.savemap)
-        #self.savesenariobuttom.connect()
+        self.savesenariobuttom.clicked.connect(self.cliplayer)
 
         # report
-        #self.statistics1table.connect()
-        #self.statistics2table.connect()
-        #self.saveStatisticsButtom.connect()
+        self.getinfo.clicked.connect(self.getsummary)
+        #self.displayStyleButton.clicked.connect(self.plotChart)
+        self.updateAttribute.connect(self.extractAttributeSummary('bu_naam', 'iso_area'))
+
 
 
     def closeEvent(self, event):
@@ -325,25 +329,6 @@ class SpatialDecisionDockWidget(QtGui.QDockWidget, FORM_CLASS):
     def sliderMoved2(self, value):
         self.sliderValue_2.setText(str(value))
 
-        # filters the network nodes and shows/keeps only those that have frequency lower or equal than the threshold according to the setTimeSlot(self)
-    def sliderValueChanged(self):
-        value = self.sliderValue_2.text()
-        if self.setTimeSlot() == 'Weekdays - Afternoon Rush Hours' and value > 76:
-            msgBox = QtGui.QMessageBox()
-            msgBox.setText(
-                "Maximum frequency value for the selected timeslot is 76.\nChoose a lower (valid) threshold")
-            msgBox.addButton(QtGui.QPushButton('Ok'), QtGui.QMessageBox.RejectRole)
-            message = msgBox.exec_()
-            if message == 0:
-                return
-        elif self.setTimeSlot() == 'Weekdays - Morning Rush Hours' and value > 96:
-            msgBox = QtGui.QMessageBox()
-            msgBox.setText(
-                "Maximum frequency value for the selected timeslot is 96.\nChoose a lower (valid) threshold")
-            msgBox.addButton(QtGui.QPushButton('Ok'), QtGui.QMessageBox.RejectRole)
-            message = msgBox.exec_()
-            if message == 0:
-                return
 
     def warningFrequencyNodes(self):
             # check if layer already exists
@@ -408,10 +393,38 @@ class SpatialDecisionDockWidget(QtGui.QDockWidget, FORM_CLASS):
 
         uf.insertTempFeatures(freq_layer, tuple[0], tuple[1])
 
-    def setAgeGroup(self):
-        pass
+    def MaxThresholdText(self):
+        timeslot_field = self.setTimeSlot()
+        if timeslot_field == 'Rh1_Wd/h':
+            self.label_10.setText('96 is the maximum frequency per hour value that a mode(s) pass\n from a node for the selected timeslot')
+        elif timeslot_field == 'Rh2_Wd/h':
+            self.label_10.setText('76 is the maximum frequency per hour value that a mode(s) pass\n from a node for the selected timeslot')
+        elif timeslot_field == 'NRh_Wd/h':
+            self.label_10.setText('58 is the maximum frequency per hour value that a mode(s) pass\n from a node for the selected timeslot')
+        elif timeslot_field == 'Rh1_We/h':
+            self.label_10.setText('72 is the maximum frequency per hour value that a mode(s) pass\n from a node for the selected timeslot')
+        elif timeslot_field == 'Rh2_We/h':
+            self.label_10.setText('58 is the maximum frequency per hour value that a mode(s) pass\n from a node for the selected timeslot')
+        elif timeslot_field == 'NRh_We/h':
+            self.label_10.setText('34 is the maximum frequency per hour value that a mode(s) pass\n from a node for the selected timeslot')
+
 
         # analysis functions
+
+    def warningbuffer(self):
+
+        # check if layer already exists
+        layer = uf.getLegendLayerByName(self.iface,'Buffers')
+        if layer:
+            msgBox = QtGui.QMessageBox()
+            msgBox.setText("The Buffer Layer is already calculated for this scenario, overwrite current layer?")
+            msgBox.setStandardButtons(QtGui.QMessageBox.Yes)
+            msgBox.addButton(QtGui.QMessageBox.No)
+            msgBox.setDefaultButton(QtGui.QMessageBox.No)
+            if msgBox.exec_() == QtGui.QMessageBox.Yes:
+                self.calculateBuffer()
+        else:
+            self.calculateBuffer()
 
     def savemap(self):
         path = QtGui.QFileDialog.getSaveFileName(self, 'Save File', '', 'PNG(*.png)')
@@ -472,10 +485,11 @@ class SpatialDecisionDockWidget(QtGui.QDockWidget, FORM_CLASS):
 
     def calculateBuffer(self):
         # store the buffer results in temporary layer called "Buffers"
+        old_layer = uf.getLegendLayerByName(self.iface, "Buffers")
+        if old_layer:
+            #ids = uf.getAllFeatureIds(old_layer)
+            QgsMapLayerRegistry.instance().removeMapLayer(old_layer.id())
         buffer_layer = uf.getLegendLayerByName(self.iface, "Buffers")
-        if buffer_layer:
-            ids = uf.getAllFeatureIds(buffer_layer)
-            buffer_layer.deleteFeatures(ids)
         layer = self.getSelectedLayer()
         origins = layer.getFeatures()
         if origins > 0:
@@ -509,13 +523,220 @@ class SpatialDecisionDockWidget(QtGui.QDockWidget, FORM_CLASS):
         return layer
 
     # show buffers with nodes inside in order to delete them
-    def getbufferwithnodes(self):
-        NetworkNodes_layer = uf.getLegendLayerByName(self.iface, "PT Network Nodes")
-        self.calculateBuffer()
+    def getfeaturesforinitialareas(self):
+        NetworkNodes_layer = uf.getLegendLayerByName(self.iface, "base_nodes_filtered")
         buffer = uf.getLegendLayerByName(self.iface, 'Buffers')
+        bufferfeatures=buffer.getFeatures()
 
         features = uf.getFeaturesByIntersection(buffer, NetworkNodes_layer, True)
-        buffer.deleteFeature(features)
-        buffer.updateFields()
+        #print len(features)
 
-        return features
+        values = []
+
+        for i in features:
+            # buffer.deleteFeature(i.sid())
+            # column = i.attribute("sid")
+            values.append(i.attribute('sid'))
+        #print values
+
+
+        geometrybuffer=[]
+        attributes=[]
+
+        for i in bufferfeatures:
+            column =i.attribute('sid')
+            if column not in values:
+
+                val = []
+                val.append(i.attribute('sid'))
+                val.append(i.attribute('inw2014'))
+                val.append(i.attribute('popden10m2'))
+                attributes.append(val)
+                geometrybuffer.append(i.geometry().asPolygon())
+
+        return attributes, geometrybuffer
+
+    def layer_initial(self):
+        layer=uf.getLegendLayerByName(self.iface,'Buffers')
+        # delete old layer if present
+        old_layer = uf.getLegendLayerByName(self.iface,'initial_areas')
+        if old_layer:
+            QgsMapLayerRegistry.instance().removeMapLayer(old_layer.id())
+
+            # create one if it doesn't exist and add suffix for the scenario
+        initial_areas = uf.getLegendLayerByName(self.iface, 'initial_areas')
+        if not initial_areas:
+            attribs = ['sid', 'inw2014', 'popden10m2']
+            types = [QtCore.QVariant.Double, QtCore.QVariant.Double, QtCore.QVariant.Double]
+            initial_areas = uf.createTempLayer('initial_areas', 'POLYGON', layer.crs().postgisSrid(), attribs, types)
+            initial_areas.setLayerName('initial_areas')
+            uf.loadTempLayer(initial_areas)
+            legend = self.iface.legendInterface()
+            legend.setLayerVisible(initial_areas, True)
+
+            # insert pointsgeom & values
+        tuple = self.getfeaturesforinitialareas()
+
+        uf.insertTempFeatures(initial_areas, tuple[1], tuple[0])
+
+
+    def filter_with_population(self):
+        initialLayer = uf.getLegendLayerByName(self.iface, "initial_areas")
+        areas = initialLayer.getFeatures()
+        threshold = uf.convertNumeric(self.populationthreshold.text())
+        filtered_areas = []
+        attributes = []
+        for feature in areas:
+            # print feature
+            column = feature.attribute('inw2014')
+            # print column
+            if column > threshold:
+                values = []
+                values.append(feature.attribute('sid'))
+                values.append(feature.attribute('inw2014'))
+                values.append(feature.attribute('popden10m2'))
+
+                attributes.append(values)
+                filtered_areas.append(feature.geometry().asPolygon())
+
+        return filtered_areas, attributes
+
+    def layerpopulation(self):
+        # delete old layer if present
+        current_scenario = self.scenarioCombo.currentText()
+        old_layer = uf.getLegendLayerByName(self.iface, current_scenario + '_critical_areas')
+        if old_layer:
+            QgsMapLayerRegistry.instance().removeMapLayer(old_layer.id())
+
+            # create one if it doesn't exist and add suffix for the scenario
+        Layer = uf.getLegendLayerByName(self.iface, 'initial_areas')
+        current_scenario = self.scenarioCombo.currentText()
+        critical_layer = uf.getLegendLayerByName(self.iface, current_scenario + '_critical_areas')
+        if not critical_layer:
+            timeslot_field = self.setTimeSlot()
+            attribs = ['sid', 'inw2014', 'popden10m2']
+            types = [QtCore.QVariant.Int, QtCore.QVariant.Double, QtCore.QVariant.Double]
+            critical_layer = uf.createTempLayer(current_scenario + '_critical_areas', 'POLYGON',Layer.crs().postgisSrid(), attribs, types)
+            critical_layer.setLayerName(current_scenario + '_critical_areas')
+            uf.loadTempLayer(critical_layer)
+
+            # insert pointsgeom & values
+        tuple = self.filter_with_population()
+
+        uf.insertTempFeatures(critical_layer, tuple[0], tuple[1])
+
+        self.cliplayer()
+
+
+    def cliplayer(self):
+        current_scenario = self.scenarioCombo.currentText()
+        overlay_layer = uf.getLegendLayerByName(self.iface,'Neighbourhoods population')
+        to_clip= uf.getLegendLayerByName(self.iface, current_scenario + '_critical_areas')
+
+        result = processing.runandload("qgis:clip", overlay_layer , to_clip ,None)
+
+        clip_layer = uf.getLegendLayerByName(self.iface, "Clipped")
+
+        prov= clip_layer.dataProvider()
+        areas = [feat.geometry().area() for feat in clip_layer.getFeatures()]
+        field = QgsField('iso_area', QtCore.QVariant.Double)
+        prov.addAttributes([field])
+        clip_layer.updateFields()
+
+        idx = clip_layer.fieldNameIndex('iso_area')
+
+        for area in areas:
+            new_values = {idx: float(round(area,4))}
+            prov.changeAttributeValues({areas.index(area): new_values})
+
+
+        #report functions
+    def getsummary(self):
+        self.textagegroup.clear()
+        x=self.selectCensusCombo.currentText()
+        self.textagegroup.insertItem(0, x)
+
+        self.texttimeslot.clear()
+        y=self.selecttimeCombo.currentText()
+        self.texttimeslot.insertItem(0, y)
+
+        self.textfreq.clear()
+        i=self.sliderValue_2.text()
+        self.textfreq.insertItem(0,i)
+
+        self.textbufferdist.clear()
+        z=self.sliderValue.text()
+        self.textbufferdist.insertItem(0, z)
+
+        self.popthreshold.clear()
+        d=self.populationthreshold.text()
+        self.popthreshold.insertItem(0, d)
+
+    def plotChart(self):
+        iso_layer = uf.getLegendLayerByName(self.iface, "Clipped")
+        area_list = []
+        names_list = []
+        if iso_layer:
+            D = {}
+            iso_attrs = iso_layer.getFeatures()
+            for feature in iso_attrs:
+                # retrieve every feature with its geometry and attributes
+                # fetch geometry
+                area = feature.iso_area()
+                area_list.append(area)
+                name = feature.bu_naam()
+                names_list.append(name)
+
+                D[name] = area
+                mpl_fig = plt.figure()
+                ax = mpl_fig.add_subplot(111)
+                ax.set_ylabel('Isolated area - neighbourhoud name')
+                ax.set_xlabel('Area m2')
+                ax.set_title('Non covered from the Network areas')
+
+                plt.bar(range(len(D)), D.values(), align='center')
+                plt.xticks(range(len(D)), D.keys())
+
+                plot_url = py.plot_mpl(mpl_fig, filename='mpl-dictionary')
+
+                # draw all the plots
+                self.chart_canvas.draw()
+            else:
+                self.clearChart()
+
+
+    def extractAttributeSummary(self, attribute1, attribute2):
+        # get summary of the attribute
+        layer = uf.getLegendLayerByName(self.iface, "Clipped")
+        summary = []
+        # only use the first attribute in the list
+        for feature in layer.getFeatures():
+            summary.append(( feature.attribute(attribute1), feature.attribute(attribute2)))
+        # send this to the table
+        self.clearTable()
+        self.updateTable(summary)
+
+    def clearTable(self):
+        self.statisticsTable.clear()
+
+
+    def updateTable(self, values):
+        # takes a list of label / value pairs, can be tuples or lists. not dictionaries to control order
+        self.statisticsTable.setColumnCount(3)
+        self.statisticsTable.setHorizontalHeaderLabels(['bu_naam', 'iso_area'])
+        self.statisticsTable.setRowCount(len(values))
+        for i, item in enumerate(values):
+            # i is the table row, items mus tbe added as QTableWidgetItems
+            self.statisticsTable.setItem(i, 0, QtGui.QTableWidgetItem(str(item[0])))
+            self.statisticsTable.setItem(i, 1, QtGui.QTableWidgetItem(str(item[1])))
+        self.statisticsTable.horizontalHeader().setResizeMode(0, QtGui.QHeaderView.ResizeToContents)
+        self.statisticsTable.horizontalHeader().setResizeMode(1, QtGui.QHeaderView.Stretch)
+        self.statisticsTable.resizeRowsToContents()
+
+
+
+
+
+
+
+
